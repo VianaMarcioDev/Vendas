eb4814f38adab6510e057d67d72c5aed3bf1b38e
Márcio de Freitas Viana <marcio.viana@zup.com.br>
2021-12-27 22:00:49 -0300

    Use dependency injection in service class

--- src/main/java/br/com/zup/repository/ClientRepository.java
+++ src/main/java/br/com/zup/repository/ClientRepository.java
@@ -1,7 +1,9 @@
 package br.com.zup.repository;
 
 import br.com.zup.model.Client;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class ClientRepository {
     public void persist(Client client) {
     }
--- src/main/java/br/com/zup/service/ClientService.java
+++ src/main/java/br/com/zup/service/ClientService.java
@@ -2,13 +2,22 @@ package br.com.zup.service;
 
 import br.com.zup.model.Client;
 import br.com.zup.repository.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class ClientService {
+
+    private ClientRepository repository;
+
+    @Autowired
+    public ClientService(ClientRepository repository){
+        this.repository = repository;
+    }
 
     public void saveClient(Client client){
     validateClient(client);
-        ClientRepository clientRepository = new ClientRepository();
-        clientRepository.persist(client);
+        this.repository.persist(client);
     }
 
     public void validateClient(Client client){

f05752265b6c87c2436592f8116dc7a8eb71f0a6
Márcio de Freitas Viana <marcio.viana@zup.com.br>
2021-12-28 19:56:43 -0300

    Add method listarTodosClientes na classe ClientesRepository

--- src/main/java/br/com/zup/VendasApplication.java
+++ src/main/java/br/com/zup/VendasApplication.java
@@ -1,10 +1,24 @@
 package br.com.zup;
 
+import br.com.zup.domain.Cliente;
+import br.com.zup.repository.ClientesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class VendasApplication {
+
+    @Bean
+    public CommandLineRunner init(@Autowired ClientesRepository clientesRepository){
+        return args -> {
+            Cliente cliente = new Cliente();
+            cliente.setNome("Márcio");
+            clientesRepository.salvar(cliente);
+        };
+    }
 
    public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);
--- src/main/java/br/com/zup/repository/ClientesRepository.java
+++ src/main/java/br/com/zup/repository/ClientesRepository.java
@@ -3,12 +3,18 @@ package br.com.zup.repository;
 import br.com.zup.domain.Cliente;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
 
 @Repository
 public class ClientesRepository {
 
-    private static String INSERT = "insert into clientes (nome) values (?)";
+    private static String INSERT = "insert into cliente (nome) values (?)";
+    private static String SELECT_ALL = "select * from cliente";
 
     private final JdbcTemplate jdbcTemplate;
 
@@ -19,5 +25,14 @@ public class ClientesRepository {
     public Cliente salvar(Cliente cliente){
         jdbcTemplate.update(INSERT, new Object[]{cliente.getNome()} );
         return cliente;
+    }
+
+    public List<Cliente> listarTodosClientes(){
+        return jdbcTemplate.query(SELECT_ALL, new RowMapper<Cliente>() {
+            @Override
+            public Cliente mapRow(ResultSet rs, int rowNum) throws SQLException {
+                return new Cliente(rs.getString("nome"));
+            }
+        });
     }
 }
